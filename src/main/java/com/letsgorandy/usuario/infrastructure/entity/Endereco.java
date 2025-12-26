6025901bc413eaa39542c05d28aa8243e1991a9f
randy <raandy.gc@gmail.com>
2025-12-26 18:26:52 -0300

    Adicionadas classes de security

---
+++ src/main/java/com/letsgorandy/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,35 @@
+package com.letsgorandy.usuario.infrastructure.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "numero")
+    private Long numero;
+    @Column(name = "bairro", length = 50)
+    private String bairro;
+    @Column(name = "cidade", length = 50)
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+    @Column(name = "cep", length = 9)
+    private String cep;
+    @Column(name = "complemento")
+    private String complemento;
+
+}
